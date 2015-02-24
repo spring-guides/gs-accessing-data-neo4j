67b9d4f53588564b04dfedad2e3f5c95c040eb06
Thomas Darimont <tdarimont@pivotal.io>
2015-02-24 11:09:24 +0100

    Updated example to be able to use @SpringBootApplication.
    Simplified configuration by introducing a dedicated config class.
    The usage of @SpringBootApplication with a transitive dependency to hibernate-validator requires javax.el to be on the classpath to work.

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -1,5 +1,7 @@
 package hello;
 
+import java.io.File;
+
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Transaction;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
@@ -7,82 +9,82 @@ import org.neo4j.kernel.impl.util.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.neo4j.config.EnableNeo4jRepositories;
 import org.springframework.data.neo4j.config.Neo4jConfiguration;
 import org.springframework.data.neo4j.core.GraphDatabase;
 
-import java.io.File;
+@SpringBootApplication
+public class Application implements CommandLineRunner {
 
-@Configuration
-@EnableNeo4jRepositories(basePackages = "hello")
-public class Application extends Neo4jConfiguration implements CommandLineRunner {
+	@Configuration
+	@EnableNeo4jRepositories(basePackages = "hello")
+	static class ApplicationConfig extends Neo4jConfiguration {
 
-    public Application() {
-        setBasePackage("hello");
-    }
+		public ApplicationConfig() {
+			setBasePackage("hello");
+		}
 
-    @Bean
-    GraphDatabaseService graphDatabaseService() {
-        return new GraphDatabaseFactory().newEmbeddedDatabase("accessingdataneo4j.db");
-    }
+		@Bean
+		GraphDatabaseService graphDatabaseService() {
+			return new GraphDatabaseFactory().newEmbeddedDatabase("accessingdataneo4j.db");
+		}
+	}
 
-    @Autowired
-    PersonRepository personRepository;
+	@Autowired PersonRepository personRepository;
 
-    @Autowired
-    GraphDatabase graphDatabase;
+	@Autowired GraphDatabase graphDatabase;
 
-    public void run(String... args) throws Exception {
-        Person greg = new Person("Greg");
-        Person roy = new Person("Roy");
-        Person craig = new Person("Craig");
+	public void run(String... args) throws Exception {
 
-        System.out.println("Before linking up with Neo4j...");
-        for (Person person : new Person[]{greg, roy, craig}) {
-            System.out.println(person);
-        }
+		Person greg = new Person("Greg");
+		Person roy = new Person("Roy");
+		Person craig = new Person("Craig");
 
-        Transaction tx = graphDatabase.beginTx();
-        try {
-            personRepository.save(greg);
-            personRepository.save(roy);
-            personRepository.save(craig);
+		System.out.println("Before linking up with Neo4j...");
+		for (Person person : new Person[] { greg, roy, craig }) {
+			System.out.println(person);
+		}
 
-            greg = personRepository.findByName(greg.name);
-            greg.worksWith(roy);
-            greg.worksWith(craig);
-            personRepository.save(greg);
+		Transaction tx = graphDatabase.beginTx();
+		try {
+			personRepository.save(greg);
+			personRepository.save(roy);
+			personRepository.save(craig);
 
-            roy = personRepository.findByName(roy.name);
-            roy.worksWith(craig);
-            // We already know that roy works with greg
-            personRepository.save(roy);
+			greg = personRepository.findByName(greg.name);
+			greg.worksWith(roy);
+			greg.worksWith(craig);
+			personRepository.save(greg);
 
-            // We already know craig works with roy and greg
+			roy = personRepository.findByName(roy.name);
+			roy.worksWith(craig);
+			// We already know that roy works with greg
+			personRepository.save(roy);
 
-            System.out.println("Lookup each person by name...");
-            for (String name: new String[]{greg.name, roy.name, craig.name}) {
-                System.out.println(personRepository.findByName(name));
-            }
+			// We already know craig works with roy and greg
 
-            System.out.println("Looking up who works with Greg...");
-            for (Person person : personRepository.findByTeammatesName("Greg")) {
-                System.out.println(person.name + " works with Greg.");
-            }
+			System.out.println("Lookup each person by name...");
+			for (String name : new String[] { greg.name, roy.name, craig.name }) {
+				System.out.println(personRepository.findByName(name));
+			}
 
-            tx.success();
-        } finally {
-            tx.close();
-        }
+			System.out.println("Looking up who works with Greg...");
+			for (Person person : personRepository.findByTeammatesName("Greg")) {
+				System.out.println(person.name + " works with Greg.");
+			}
 
-    }
+			tx.success();
+		} finally {
+			tx.close();
+		}
+	}
 
-    public static void main(String[] args) throws Exception {
-        FileUtils.deleteRecursively(new File("accessingdataneo4j.db"));
+	public static void main(String[] args) throws Exception {
+		FileUtils.deleteRecursively(new File("accessingdataneo4j.db"));
 
-        SpringApplication.run(Application.class, args);
-    }
-
+		SpringApplication.run(Application.class, args);
+	}
 }
