0a3da0cb18003c6bc473b3f1aef55927bc1959a1
Zoltan Altfatter <altfatterz@gmail.com>
2016-09-20 21:07:44 +0200

    small fixes

--- complete/src/main/java/hello/Person.java
+++ complete/src/main/java/hello/Person.java
@@ -18,12 +18,6 @@ public class Person {
 
 	private String name;
 
-	/**
-	 * Neo4j requires a no-arg constructor much like JPA
-	 */
-	private Person() {
-	}
-
 	public Person(String name) {
 		this.name = name;
 	}
