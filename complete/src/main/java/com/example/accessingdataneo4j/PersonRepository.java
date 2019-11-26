ae42c3e775182f7436f6f17e2454cbdfa38f6d14
Greg Turnquist <gturnquist@pivotal.io>
2019-11-26 13:07:12 -0600

    Switch to tabs, and adjust tabsizing in asciidoctor.

--- complete/src/main/java/com/example/accessingdataneo4j/PersonRepository.java
+++ complete/src/main/java/com/example/accessingdataneo4j/PersonRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface PersonRepository extends CrudRepository<Person, Long> {
 
-    Person findByName(String name);
+	Person findByName(String name);
 }
